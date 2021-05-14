575a922eb806064cd95f5126066ee0c1d5c39e36
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-05-13 21:29:58 -0400

    added pseudocode to add a supression function to remove submit button upon click and local storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,120 +1,116 @@
 var questions = [
     {
-      question: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts",
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
     },
     {
-      question:
-        "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses",
+        question:
+            "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
     },
-  ];
+];
 
 
 //add ability to save initials and score
 // set array for saving initials into local storage
-  var scores = []
-  var questionEl = document.querySelector("#question");
-  var optionListEl = document.querySelector("#option-list");
-  var questionResultEl = document.querySelector("#question-result");
-  var timerEl = document.querySelector("#timer");
-  
-  var questionIndex = 0;
-  var correctCount = 0;
-  
-  var time = 60;
-  var intervalId;
+var scores = []
+var questionEl = document.querySelector("#question");
+var optionListEl = document.querySelector("#option-list");
+var questionResultEl = document.querySelector("#question-result");
+var timerEl = document.querySelector("#timer");
 
-  // create start button
-  var button =
-  document.createElement("button");
-  button.innerHTML = "Start Quiz";
+var questionIndex = 0;
+var correctCount = 0;
 
-  var body = document.getElementsByTagName("body")
-  [0];
-  body.appendChild(button);
+var time = 60;
+var intervalId;
+// create a function to suppress other parts from showing. ex: start quiz button once clicked is suppressed
+// create start button
+var button =
+    document.createElement("button");
+button.innerHTML = "Start Quiz";
 
-  button.addEventListener ("click", startGame);
+var body = document.getElementsByTagName("body")
+[0];
+body.appendChild(button);
 
-  // create start game function 
-  function startGame() {
+button.addEventListener("click", startGame);
+
+// create start game function 
+function startGame() {
     // call update time
-    
+
     timerEl.textContent = time;
     // updateTime();
     renderQuestion();
 
-  };
+};
 
-  
-  function endQuiz() {
+
+function endQuiz() {
     clearInterval(intervalId);
     var body = document.body;
     body.innerHTML = "Game over, You scored " + correctCount;
-  }
-  
-  function updateTime() {
+}
+
+function updateTime() {
     // if (time == 0) {
-        time--;
-        timerEl.textContent = time;
-        if (time <= 0) {
-          endQuiz();
-        }
-      return;
+    time--;
+    timerEl.textContent = time;
+    if (time <= 0) {
+        endQuiz();
     }
-  
-  
-  
-  function renderQuestion() {
-    
+    return;
+}
+
+
+
+function renderQuestion() {
+
     intervalId = setInterval(updateTime, 1000);
-    
+
     questionEl.textContent = questions[questionIndex].question;
-  
+
     optionListEl.innerHTML = "";
     questionResultEl.innerHTML = "";
-  
+
     var choices = questions[questionIndex].choices;
     var choicesLenth = choices.length;
-  
+
     for (var i = 0; i < choicesLenth; i++) {
-      var questionListItem = document.createElement("li");
-      questionListItem.textContent = choices[i];
-      optionListEl.append(questionListItem);
+        var questionListItem = document.createElement("li");
+        questionListItem.textContent = choices[i];
+        optionListEl.append(questionListItem);
     }
-  }
-  
-  function nextQuestion() {
+}
+
+function nextQuestion() {
     questionIndex++;
     if (questionIndex === questions.length) {
-      time = 0;
+        time = 0;
     }
     renderQuestion();
-  }
-  
-  function checkAnswer(event) {
+}
+
+function checkAnswer(event) {
     clearInterval(intervalId);
     if (event.target.matches("li")) {
-      var answer = event.target.textContent;
-      if (answer === questions[questionIndex].answer) {
-        questionResultEl.textContent = "Correct";
-        correctCount++;
-      } else {
-        questionResultEl.textContent = "Incorrect";
-        time = time - 2;
-        timerEl.textContent = time;
-      }
+        var answer = event.target.textContent;
+        if (answer === questions[questionIndex].answer) {
+            questionResultEl.textContent = "Correct";
+            correctCount++;
+        } else {
+            questionResultEl.textContent = "Incorrect";
+            time = time - 2;
+            timerEl.textContent = time;
+        }
     }
     setTimeout(nextQuestion, 2000);
-  }
-//   var startButton = "start-button";
-
-//   function startQuiz() {
-
-//   }
+}
+// create a local storage that connects to score = [] to save scores
 
 //   renderQuestion();
 //   startButton.addEventListener("click", startQuiz);
-  optionListEl.addEventListener("click", checkAnswer);
+optionListEl.addEventListener("click", checkAnswer);
